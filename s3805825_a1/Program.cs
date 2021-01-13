5ab6b3dd25540cd25791bbef07cf3510b31e748b
s3805825 <s3805825@student.rmit.edu.au>
2021-01-13 16:07:24 +1100

    initial commit

---
+++ s3805825_a1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace s3805825_a1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
